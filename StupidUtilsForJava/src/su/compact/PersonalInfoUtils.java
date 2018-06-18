a21ba50367f415263d4343be5c9bce10efd7d3e8
pakyin-wong <36594148+pakyin-wong@users.noreply.github.com>
2018-06-18 18:21:05 +0800

    PersonalInfoUtils

---
+++ StupidUtilsForJava/src/su/compact/PersonalInfoUtils.java
@@ -0,0 +1,15 @@
+package su.compact;
+
+public class PersonalInfoUtils {
+	public int countWords(String str){
+		if (str == null) return 0;
+		if (str.isEmpty()) return 0;
+		return 0;
+	}
+	public int countWords(String str,int... types){
+		return 0;
+	}
+	public int extractFirstName(String fullname, int type){
+		return 0;
+	}
+}
