74a84549046a619bccf8cab91784a1238011cdb7
pakyin-wong <36594148+pakyin-wong@users.noreply.github.com>
2018-06-18 18:28:15 +0800

    added count ASCII

--- StupidUtilsForJava/src/su/full/PersonalInfoUtils.java
+++ StupidUtilsForJava/src/su/full/PersonalInfoUtils.java
@@ -1,5 +1,7 @@
 package su.full;
 
+import com.google.common.base.CharMatcher;
+
 public class PersonalInfoUtils {
 	public int countWords(String str){
 		if (str == null) return 0;
@@ -8,6 +10,17 @@ public class PersonalInfoUtils {
 	}
 	public int countWords(String str, Language... languages){
 		return 0;
+	}
+	public int countASCIIChar(String str){
+		if (str == null || str.equals("")) return 0;
+		int count = 0;
+		String out = "";
+		for (int i = 0; i < str.length(); i++){
+			char chr = str.charAt(i);
+			boolean ascii = CharMatcher.ascii().matches(chr);
+			if (ascii) count++;
+		}
+		return count;
 	}
 	public int extractFirstName(String fullname, int type){
 		return 0;
